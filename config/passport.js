c2623f57388ecf1ceb731aa3f9f2efd21224a4d1
Manuel Vargas <mvargas@xpectrumtech.com>
2017-10-04 11:36:11 -0500

    Fix fb

--- config/passport.js
+++ config/passport.js
@@ -81,7 +81,7 @@ module.exports = function(passport, dev) {
     if (dev) {
         facebookCallbackURL = configAuth.facebookAuth.callbackURL;
     } else {
-        facebookCallbackURL = configAuth.facebookAuth.callbackURLPrd.replace('/','\\/'‌​);
+        facebookCallbackURL = configAuth.facebookAuth.callbackURLPrd.replace(/\//g, "\\/")
     }
 
     // FACEBOOK Strategy
